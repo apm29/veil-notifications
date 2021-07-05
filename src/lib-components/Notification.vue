2bdded693198417da6e07df71da929b57ec8dcd6
apm29 <835170486@qq.com>
2021-07-05 17:01:05 +0800

    feature:使用veil-transitions

--- src/lib-components/Notification.vue
+++ src/lib-components/Notification.vue
@@ -2,50 +2,50 @@
   <div
       class="tw-absolute tw-top-1 tw-left-1.5 tw-flex tw-flex-col tw-items-start"
   >
-    <transition-group name="slide-y-transition">
+    <VeilSlideYTransition hide-on-leave group>
       <NotificationContent
           v-for="notification of notifyQueue.filter((it) => it.group === 'tl')"
           :notification="notification"
           :key="notification.id"
       />
-    </transition-group>
+    </VeilSlideYTransition>
   </div>
   <div
       class="tw-absolute tw-top-1 tw-right-1.5 tw-flex tw-flex-col tw-items-end"
   >
-    <transition-group name="slide-y-transition">
+    <VeilSlideYTransition hide-on-leave group>
       <NotificationContent
           v-for="notification of notifyQueue.filter((it) => it.group === 'tr')"
           :notification="notification"
           :key="notification.id"
       />
-    </transition-group>
+    </VeilSlideYTransition>
   </div>
   <div
       class="
       tw-absolute tw-bottom-5 tw-left-1.5 tw-flex tw-flex-col tw-items-start
     "
   >
-    <transition-group  name="slide-y-reverse-transition">
+    <VeilSlideYReverseTransition hide-on-leave group>
       <NotificationContent
           v-for="notification of notifyQueue.filter((it) => it.group === 'bl')"
           :notification="notification"
           :key="notification.id"
       />
-    </transition-group>
+    </VeilSlideYReverseTransition>
   </div>
   <div
       class="
       tw-absolute tw-bottom-5 tw-right-1.5 tw-flex tw-flex-col tw-items-end
     "
   >
-    <transition-group  name="slide-y-reverse-transition">
+    <VeilSlideYReverseTransition hide-on-leave group>
       <NotificationContent
           v-for="notification of notifyQueue.filter((it) => it.group === 'br')"
           :notification="notification"
           :key="notification.id"
       />
-    </transition-group>
+    </VeilSlideYReverseTransition>
   </div>
 
   <div
@@ -59,13 +59,13 @@
       tw-items-center
     "
   >
-    <transition-group  name="slide-y-reverse-transition">
+    <VeilSlideYReverseTransition hide-on-leave group>
       <NotificationContent
           v-for="notification of notifyQueue.filter((it) => it.group === 'bc')"
           :notification="notification"
           :key="notification.id"
       />
-    </transition-group>
+    </VeilSlideYReverseTransition>
   </div>
   <div
       class="
@@ -78,24 +78,25 @@
       tw-items-center
     "
   >
-   <transition-group  name="slide-y-transition">
+    <VeilSlideYTransition hide-on-leave group>
       <NotificationContent
           v-for="notification of notifyQueue.filter((it) => it.group === 'tc')"
           :notification="notification"
           :key="notification.id"
       />
-    </transition-group>
+    </VeilSlideYTransition>
   </div>
 </template>
 
 <script>
 import { notifyQueue } from './notify'
 import NotificationContent from './NotificationContent.vue'
+import { VeilSlideYReverseTransition, VeilSlideYTransition } from 'veil-transitions'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'Notification',
-  components: { NotificationContent },
+  components: { NotificationContent, VeilSlideYReverseTransition, VeilSlideYTransition },
   emits: ['notification-click'],
   setup() {
     return {
--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 import "./index.css"
+import "veil-transitions/src/lib-components/index.css"
 export { default as VeilNotifications } from './Notification.vue';
 export { notification as notification } from './notify';
